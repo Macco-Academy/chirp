51df64128cb5273df280213b5534d678f6e39c73
Yianni1980 <88034690+Yianni1980@users.noreply.github.com>
2023-06-08 18:29:34 +0300

    Update Chirp/Pages/LoginPage/LoginPageViewController.swift
    Co-authored-by: Emmanuel Okwara <emma4real37@gmail.com>

--- Chirp/Pages/LoginPage/LoginPageViewController.swift
+++ Chirp/Pages/LoginPage/LoginPageViewController.swift
@@ -58,7 +58,7 @@ class LoginPageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         layout()
         
     }
