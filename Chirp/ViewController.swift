9ddda40b0b973a966705d3e015226f1d8d30aeb0
Kwaku Dapaah <kwakudapaah@Kwakus-Mac-mini.local>
2023-06-08 11:05:29 -0400

    removed button code from viewcontroller.swift

--- Chirp/ViewController.swift
+++ Chirp/ViewController.swift
@@ -7,44 +7,14 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
-    
-    let button: UIButton = {
-        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-        btn.setTitle("Navigate to Tab Screen", for: .normal)
-        btn.setTitleColor(.black, for: .normal)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-       
-        return btn
-    }()
-    
+class ViewController: UIViewController {
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .white
-        view.addSubview(button)
-        setupBtn()
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
-    
-    
-    @objc func buttonTapped() {
-        let tabBarVC = HomeTabBarController()
-        
-        tabBarVC.modalPresentationStyle = .fullScreen
-        
-        present(tabBarVC, animated: true)
-    }
-    
-    func setupBtn() {
-        
-        NSLayoutConstraint.activate([button.centerXAnchor.constraint(equalTo: view.centerXAnchor), button.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
-        
-        
-    }
-    
 }
 
 
